ef5dba993587d096b00b0374c389a4719d9ea503
Salman Akbar Hasbullah <salmanakbartafisa@gmail.com>
2023-11-12 21:29:27 +0700

    update

---
+++ Login Page/html-login-page/signup.js
@@ -0,0 +1,41 @@
+// signup.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const signupForm = document.querySelector("form");
+
+    signupForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const confirmPassword = document.getElementById("confirm-password").value;
+
+        if (password !== confirmPassword) {
+            alert("Passwords do not match. Please try again.");
+            return;
+        }
+
+        try {
+            const response = await fetch("https://us-central1-bustling-walker-340203.cloudfunctions.net/function-GISSGINUPCOY", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error("Signup failed. Please try again.");
+            }
+
+            alert("Signup successful!");
+            // Redirect or perform any other actions after successful signup
+        } catch (error) {
+            console.error("Error during signup:", error);
+            alert("Signup failed. Please try again.");
+        }
+    });
+});
