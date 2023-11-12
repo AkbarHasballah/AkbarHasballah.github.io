2b02084e79f5d6b3afe621233c3d4787b2b4876b
Salman Akbar Hasbullah <salmanakbartafisa@gmail.com>
2023-11-12 23:04:30 +0700

    testing

--- Login Page/html-login-page/signin.js
+++ Login Page/html-login-page/signin.js
@@ -1,38 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
     const usernameInput = document.getElementById("username");
     const passwordInput = document.getElementById("password");
-    const rememberCheckbox = document.getElementById("remember");
     const submitButton = document.getElementById("submit");
     const loginForm = document.getElementById("loginForm");
 
     const loginApiUrl = "https://us-central1-bustling-walker-340203.cloudfunctions.net/function-SIGNREGISGISCOY";
-    const dashboardUrl = "./dashboard/dashboard.html";
 
     loginForm.addEventListener("submit", async (event) => {
-        event.preventDefault();
+      event.preventDefault();
 
-        const username = usernameInput.value;
-        const password = passwordInput.value;
-        const remember = rememberCheckbox.checked;
+      const username = usernameInput.value;
+      const password = passwordInput.value;
 
-        try {
-            const response = await fetch(loginApiUrl, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ username, password, remember }),
-            });
+      try {
+        const response = await fetch(loginApiUrl, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        });
 
-            if (response.ok) {
-                window.location.href = dashboardUrl;
-            } else {
-                const data = await response.json();
-                throw new Error(data.message);
-            }
-        } catch (error) {
-            console.error("Error during login:", error);
-            // Handle error, display message, etc.
+        if (response.ok) {
+          alert("Login successful!");
+          window.location.href = "./dashboard/dashboard.html";
+        } else {
+          const data = await response.json();
+          throw new Error(data.message);
         }
+      } catch (error) {
+        console.error("Error during login:", error);
+        // Handle error, e.g., display an error message
+      }
     });
-});
+  });
