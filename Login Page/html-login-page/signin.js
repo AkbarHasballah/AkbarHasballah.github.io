78ef712442f9c0cdab70d5873a619000d9cd6649
Salman Akbar Hasbullah <salmanakbartafisa@gmail.com>
2023-11-12 22:58:13 +0700

    tesing

--- Login Page/html-login-page/signin.js
+++ Login Page/html-login-page/signin.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const usernameInput = document.getElementById("username");
     const passwordInput = document.getElementById("password");
+    const rememberCheckbox = document.getElementById("remember");
     const submitButton = document.getElementById("submit");
-    const registerForm = document.getElementById("loginForm");
-    const errorMessage = document.getElementById("error-message");
+    const loginForm = document.getElementById("loginForm");
 
     const loginApiUrl = "https://us-central1-bustling-walker-340203.cloudfunctions.net/function-SIGNREGISGISCOY";
+    const dashboardUrl = "./dashboard/dashboard.html";
 
-    registerForm.addEventListener("submit", async (event) => {
+    loginForm.addEventListener("submit", async (event) => {
         event.preventDefault();
 
         const username = usernameInput.value;
         const password = passwordInput.value;
-
-        errorMessage.textContent = ""; // Clear previous error message
+        const remember = rememberCheckbox.checked;
 
         try {
             const response = await fetch(loginApiUrl, {
@@ -21,20 +21,18 @@ document.addEventListener("DOMContentLoaded", () => {
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ username, password }),
+                body: JSON.stringify({ username, password, remember }),
             });
 
             if (response.ok) {
-                alert("Login successful!");
-                // Redirect to dashboard.html
-                window.location.href = "../dashboard/dashboard.html";
+                window.location.href = dashboardUrl;
             } else {
                 const data = await response.json();
                 throw new Error(data.message);
             }
         } catch (error) {
             console.error("Error during login:", error);
-            errorMessage.textContent = error.message;
+            // Handle error, display message, etc.
         }
     });
 });
